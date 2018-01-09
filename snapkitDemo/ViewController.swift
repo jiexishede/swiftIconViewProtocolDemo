888cdd06ab1be0b3baffa81452017076258205fa
yian <yianwork@yians-iMac.local>
2018-01-10 01:00:46 +0800

    SO更正后版本

--- snapkitDemo/ViewController.swift
+++ snapkitDemo/ViewController.swift
@@ -12,17 +12,13 @@ import SnapKit
 class ViewController: UIViewController {
     
     let arrangeStackView = UIStackView()
-
-    let followUpActionView = YAIconView()
-    let developCurriculumView = YAIconView()
-    let moreMoreView = YAIconView()
-    let takeCareSalonView = YAIconView()
+    let followUpActionView = UIView()
+    let developCurriculumView = UIView()
+    let moreMoreView = UIView()
+    let takeCareSalonView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //        let box = YAIconView(frame: view.bounds)
         
         view.addSubview(arrangeStackView)
         
@@ -31,45 +27,31 @@ class ViewController: UIViewController {
         takeCareSalonView.translatesAutoresizingMaskIntoConstraints = false;
         moreMoreView.translatesAutoresizingMaskIntoConstraints = false;
 
-        
-        
-        
         developCurriculumView.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1)
         followUpActionView.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1)
         takeCareSalonView.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1)
         moreMoreView.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1)
-        
-        
-        //  无效
-//        arrangeStackView.backgroundColor = UIColor.red
+    
+
         arrangeStackView.axis = .horizontal
         arrangeStackView.distribution = .fillEqually
-//        arrangeStackView.distribution = .equalSpacing
-        arrangeStackView.spacing = 10
+
+        arrangeStackView.spacing = 0
         arrangeStackView.alignment = .fill
-        // view 貌似是不在关注 frame
+
         arrangeStackView.translatesAutoresizingMaskIntoConstraints = false
-        arrangeStackView.addSubview(followUpActionView)
-        arrangeStackView.addSubview(developCurriculumView)
-        arrangeStackView.addSubview(moreMoreView)
-        arrangeStackView.addSubview(takeCareSalonView)
+        
+        arrangeStackView.addArrangedSubview(followUpActionView)
+        arrangeStackView.addArrangedSubview(developCurriculumView)
+        arrangeStackView.addArrangedSubview(moreMoreView)
+        arrangeStackView.addArrangedSubview(takeCareSalonView)
+
         
         arrangeStackView.snp.makeConstraints { (make) in
             make.center.equalToSuperview()
             make.height.equalTo(95)
             make.width.equalToSuperview()
         }
-        
-        
-        
-//  这个方法
-//
-//        followUpActionView.snp.makeConstraints { (make) in
-//            make.top.bottom.left.right.equalToSuperview()
-//            //             make.edges.equalToSuperview()
-//        }
-////        print(box.frame)
-        // result (0.0, 0,0, 0,0, 0,0)
         
         let yaIconWith = self.view.frame.width * 0.25
         
@@ -89,12 +71,7 @@ class ViewController: UIViewController {
             make.height.equalToSuperview()
             make.width.equalTo(yaIconWith)
         }
-        
-//        followUpActionView.iconViewSetImageAndLabel(iconName: "home-sfxd", tipTitle: "随访行动")
-//        developCurriculumView.iconViewSetImageAndLabel(iconName: "home-hjzl", tipTitle: "患教资料")
-//        moreMoreView.iconViewSetImageAndLabel(iconName: "home-gd", tipTitle: "更多")
-//        takeCareSalonView.iconViewSetImageAndLabel(iconName: "home-gasl", tipTitle: "关爱沙龙")
-        
+    
     
     }
 
